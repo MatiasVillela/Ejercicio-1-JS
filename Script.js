756d84a468a5e1f04b3a4b0df8d098d76ae03f30
mvillela319 <mvillela319@gmail.com>
2020-08-09 01:04:57 -0400

    terminado

--- Script.js
+++ Script.js
@@ -3,13 +3,13 @@ window.onload = function (){
     let excuse = document.querySelector('#excuse');
     console.log(excuse.innerHTML)
     var x = Math.floor((Math.random() * 10));
-    if(x<4){
+    if(x<5){
 excuse.innerHTML = mitexto(x);}
 else {excuse.innerHTML = mitexto(1)}}
 
 
-let who = ['Mi perro','Mi abuela','Mi Tortuga','Mi Pajarillo'];
-let what = ['se lo comió','lo chocó','lo embarró','lo quebró'];
+let who = ['Mi perro','Mi abuela','Mi Tortuga','Mi Pajarillo','Mi hermano'];
+let what = ['se lo comió','lo chocó','lo embarró','lo quebró','lo regaló'];
 let when = ['justo antes de clases','en el minuto de la entrega','cuando lo estaba terminando','mientras almorzaba'];
 
 function mitexto(indice){
